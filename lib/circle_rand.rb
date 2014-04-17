a12661a2691e7f93e8e210bc8078e347f02fe7db
Michael King <kingmt@gmail.com>
2014-04-17 14:23:38 -0500

    building up the base

--- lib/circle_rand.rb
+++ lib/circle_rand.rb
@@ -1,3 +1,9 @@
 module CircleRand
   extend self
+
+  def random_point radius=nil
+    if radius
+      [0,0]
+    end
+  end
 end
--- spec/circle_rand_spec.rb
+++ spec/circle_rand_spec.rb
@@ -1,4 +1,35 @@
 require 'circle_rand'
 
 describe CircleRand do
+  describe '#random_point' do
+    context 'when no radius specified' do
+      let(:result) { CircleRand.random_point }
+      subject { result }
+
+      it { should be_nil }
+      it 'returns 0,0'
+    end
+
+    context 'when given a radius' do
+      let(:result) { CircleRand.random_point 10 }
+      subject { result }
+
+      context 'the result' do
+      it { should be_kind_of Array }
+      its(:size) { should == 2 }
+
+      context 'distance' do
+        subject { result.first }
+        it { should >= 0 }
+        it { should <= 10 }
+      end
+
+      context 'radian' do
+        subject { result.last }
+        it { should >= 0 }
+        it { should <= 2*Math::PI }
+      end
+      end
+    end
+  end
 end
