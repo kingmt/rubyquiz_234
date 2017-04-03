7316d7ca0acc88a7db5cab777f0751a5ac4a1fd8
Michael King <kingmt@gmail.com>
2017-04-02 20:12:38 -0500

    alternate method to randomize distance from center

--- lib/circle_rand.rb
+++ lib/circle_rand.rb
@@ -1,10 +1,23 @@
 module CircleRand
   extend self
 
+  def generate_random_vector_length_v1 radius
+    Math.sqrt(Kernel.rand)*radius
+  end
+
+  def generate_random_vector_length_v2 radius
+    u = (rand + rand) * radius
+    if u > radius
+      2.0 * radius - u
+    else
+      u
+    end
+  end
+
   def random_point radius=nil
     if radius
       # distance
-      [Math.sqrt(Kernel.rand)*radius,
+      [generate_random_vector_length_v2(radius),
        # radians
        Kernel.rand*2*Math::PI]
     end
